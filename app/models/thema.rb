fdcb73ed2ddb69d16fdaf2f7ac087376b405a558
motoya <motoyao0118@gmail.com>
2022-09-19 20:21:17 +0900

    Create Question Model

--- app/models/thema.rb
+++ app/models/thema.rb
@@ -1,4 +1,6 @@
 class Thema < ApplicationRecord
   validates :name, presence: true, length: { maximum: 60 }
   validates :purpose, length: { maximum: 255 }
+  has_many :questions, dependent: :destroy
+  accepts_nested_attributes_for :questions, allow_destroy: true , reject_if: lambda {|attributes| attributes['question'].blank?}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_19_102756) do
+ActiveRecord::Schema.define(version: 2022_09_19_110545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.string "question", null: false
+    t.bigint "thema_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["thema_id"], name: "index_questions_on_thema_id"
+  end
 
   create_table "themas", force: :cascade do |t|
     t.string "name", null: false
@@ -23,4 +31,5 @@ ActiveRecord::Schema.define(version: 2022_09_19_102756) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "questions", "themas"
 end
