3c6545d223b3e1e6bbea6d7dd28fda41de18de7c
motoya <motoyao0118@gmail.com>
2022-09-28 17:36:04 +0900

    User connect to other model #15

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ApplicationRecord
   belongs_to :thema
+  belongs_to :user
   has_many :large_groups, ->{ order(position: :asc) }, dependent: :destroy
 end
---
+++ spec/factories/user_answers.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user_answer do
+    user { nil }
+    answer { nil }
+  end
+end
