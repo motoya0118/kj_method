3eb65a180bd7fa30d53802631a07df92b36a70f5
motoya <motoyao0118@gmail.com>
2022-10-01 17:49:49 +0900

    Create GuestLoginFunction #21

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks"
 }
 
+devise_scope :user do
+  post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  post 'users/admin_sign_in', to: 'users/sessions#admin_sign_in'
+end
+
   resources :themas, except:[:index] do
     member do
       get 'confirm'
