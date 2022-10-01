54bd7385b067a065e8bc939da0eec2c2ed55940c
motoya <motoyao0118@gmail.com>
2022-10-01 18:01:56 +0900

    Add ja.yml #21

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module KjMethod
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.autoload_paths += %W(#{config.root}/lib)
-    config.i18n.default_locale = :ja
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
