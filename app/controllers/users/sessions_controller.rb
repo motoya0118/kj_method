4b6b5d45cb2f15e33fa6d5577f13873f98ed9276
motoya <motoyao0118@gmail.com>
2022-10-03 18:39:58 +0900

    Fix session controller #35

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,11 +2,11 @@ class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to mypage_path, notice: 'ゲストユーザーとしてログインしました。'
   end
   def admin_sign_in
     user = User.admin_user
     sign_in user
-    redirect_to root_path, notice: 'Adminユーザーとしてログインしました。'
+    redirect_to mypage_path, notice: 'Adminユーザーとしてログインしました。'
   end
 end
