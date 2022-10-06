469011074024dc269b310ca7e4c4cd5e7744b42c
motoya <motoyao0118@gmail.com>
2022-10-06 18:24:21 +0900

    fix false redirect #49

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, kind: "twitter") if is_navigational_format?
     else
       session['devise.twitter_data'] = request.env['omniauth.auth'].except("extra")
-      redirect_to new_user_registration_url
+      redirect_to login_path, notice: "ログインに失敗しました"
     end
   end
 end
