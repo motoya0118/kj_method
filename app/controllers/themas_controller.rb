4373c2a160a3d4dc3729c6d0c0788df5681444de
motoya <motoyao0118@gmail.com>
2022-10-11 11:45:04 +0900

    fix I18n #53

--- app/controllers/themas_controller.rb
+++ app/controllers/themas_controller.rb
@@ -24,7 +24,7 @@ class ThemasController < ApplicationController
 
     if @thema.save
       session['thema'] = @thema.id
-      redirect_to thema_url(@thema), notice: "Thema was successfully created." 
+      redirect_to thema_url(@thema), notice: t("Thema was successfully created.")
     else
       render :new, status: :unprocessable_entity
     end
@@ -33,7 +33,7 @@ class ThemasController < ApplicationController
 
   def update
     if @thema.update(thema_params)
-      redirect_to thema_url(@thema), notice: "Thema was successfully updated." 
+      redirect_to thema_url(@thema), notice: t("Thema was successfully updated.")
     else
       render :edit, status: :unprocessable_entity
     end
@@ -42,7 +42,7 @@ class ThemasController < ApplicationController
   def destroy
     @thema.destroy
     session['thema'] = nil
-    redirect_to mypage_path, notice: "Thema was successfully destroyed."
+    redirect_to mypage_path, notice: t("Thema was successfully destroyed.")
   end
 
   def confirm
