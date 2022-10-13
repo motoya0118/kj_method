dd33f0109fc3f9dbd6ff16c6eed316c7bfd0b0cc
motoya <motoyao0118@gmail.com>
2022-10-13 13:25:26 +0900

    remove guest_func change top_movie

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,11 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks"
 }
 
-devise_scope :user do
-  post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
-  post 'users/admin_sign_in', to: 'users/sessions#admin_sign_in'
-end
+# #ゲスト機能
+# devise_scope :user do
+#   post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+#   post 'users/admin_sign_in', to: 'users/sessions#admin_sign_in'
+# end
 
   resources :themas, except:[:index] do
     member do
