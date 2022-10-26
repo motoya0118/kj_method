60dbd6f4b9cb37b9af938bb25a5c4dd5f9161955
motoya <motoyao0118@gmail.com>
2022-10-26 15:30:46 +0900

    due to cant signin no email user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: %i(twitter)
+         :recoverable, :rememberable,:omniauthable, omniauth_providers: %i(twitter)
   
   has_many :places
   has_many :themas, dependent: :destroy
